527f43553a3f9ffbf09f56b7a8f76c77f2f11052
vboxuser <vboxuser@noblefork.myguest.virtualbox.org>
2024-09-05 11:06:55 +0530

    proto related changes

--- x/fiattokenfactory/types/query.pb.go
+++ x/fiattokenfactory/types/query.pb.go
@@ -1727,6 +1727,7 @@ func _Query_MintingDenom_Handler(srv interface{}, ctx context.Context, dec func(
 	return interceptor(ctx, in, info, handler)
 }
 
+var Query_serviceDesc = _Query_serviceDesc
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "fiattokenfactory.Query",
 	HandlerType: (*QueryServer)(nil),
